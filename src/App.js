05290afa94ed0649e695e259f240b223867fbdf6
[사용자 <internetot@naver.com>
2018-01-11 22:24:27 +0900

    props 작동 TEST

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacker",
+  "Oldboy",
+  "Star Wars"
+]
+
+const movieImages = [
+  "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
+  "http://athenacinema.com/wp-content/uploads/2013/12/FMJ.jpg",
+  "https://movieswithaplottwist.com/wp-content/uploads/2016/04/old-boy-movie-poster.jpg",
+  "https://gemr.com/wp-content/uploads/2017/12/resize.jpeg"
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
+        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component {
     render() {
        return (
          <div>
-             <MoviePoster/>
-             <h1>Hello this is a movie</h1>
+             <MoviePoster poster={this.props.poster}/>
+             <h1>{this.props.title}</h1>
          </div>
         );
     }
@@ -15,7 +15,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
     render() {
         return (
-           <img src="https://cdn.empireonline.com/jpg/80/0/0/1000/563/0/north/0/0/0/0/0/t/films/131631/images/fUn5I5f4069vwGFEEvA3HXt9xPP.jpg" />
+           <img src={this.props.poster} />
         );
     }
 }
