e3dba23483b149f157fa11d99c65aabf38fc131a
[사용자 <internetot@naver.com>
2018-01-26 06:12:45 +0900

    Lists with .map

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacker",
-  "Oldboy",
-  "Star Wars"
-]
-
-const movieImages = [
-  "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
-  "http://athenacinema.com/wp-content/uploads/2013/12/FMJ.jpg",
-  "https://movieswithaplottwist.com/wp-content/uploads/2016/04/old-boy-movie-poster.jpg",
-  "https://gemr.com/wp-content/uploads/2017/12/resize.jpeg"
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+  },
+  {
+    title: "Full Metal Jacker",
+    poster: "http://athenacinema.com/wp-content/uploads/2013/12/FMJ.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://movieswithaplottwist.com/wp-content/uploads/2016/04/old-boy-movie-poster.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://gemr.com/wp-content/uploads/2017/12/resize.jpeg"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+        {movies.map(movie => {
+          return <Movie title={movie.title} poster={movie.poster}/>
+        })}
       </div>
     );
   }
