d36c30ecf959a68eaaf1ae442ec6692aa37ebb9b
[사용자 <internetot@naver.com>
2018-01-26 09:21:41 +0900

    데이터있으면 불러오고 없으면 Loading 표시 (State)

--- src/App.js
+++ src/App.js
@@ -7,49 +7,44 @@ class App extends Component {
   // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate -> render() -> component
 
   state = {
-      greeting: 'Hello',
-      movies: [
-          {
-              title: "Matrix",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-          },
-          {
-              title: "Full Metal Jacker",
-              poster: "http://athenacinema.com/wp-content/uploads/2013/12/FMJ.jpg"
-          },
-          {
-              title: "Oldboy",
-              poster: "https://movieswithaplottwist.com/wp-content/uploads/2016/04/old-boy-movie-poster.jpg"
-          },
-          {
-              title: "Star Wars",
-              poster: "https://gemr.com/wp-content/uploads/2017/12/resize.jpeg"
-          }
-      ]
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        movies: [
-          ...this.state.movies,
-          {
-              title:"Trainspotting",
-              poster: "https://gemr.com/wp-content/uploads/2017/12/resize.jpeg"
-          }
-        ]
+          movies: [
+              {
+                  title: "Matrix",
+                  poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+              },
+              {
+                  title: "Full Metal Jacker",
+                  poster: "http://athenacinema.com/wp-content/uploads/2013/12/FMJ.jpg"
+              },
+              {
+                  title: "Oldboy",
+                  poster: "https://movieswithaplottwist.com/wp-content/uploads/2016/04/old-boy-movie-poster.jpg"
+              },
+              {
+                  title: "Star Wars",
+                  poster: "https://gemr.com/wp-content/uploads/2017/12/resize.jpeg"
+              }
+          ]
       })
     }, 5000)
+  }
 
+  _renderMovies = () => {
+      const movies = this.state.movies.map((movie, index) => {
+        return <Movie title={movie.title} poster={movie.poster} key={index} />
+      })
+      return movies
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+          {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
